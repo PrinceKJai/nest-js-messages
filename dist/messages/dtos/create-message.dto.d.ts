d26bd2ae8566af8061aec18e9011b42fe28f8b5c
Prince Jaiswal <princejaiswal278@gmail.com>
2026-03-19 12:51:20 +0530

    removed dist and updated msg controller to use repo

--- dist/messages/dtos/create-message.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class CreateMessageDTO {
-    content: string;
-}
--- dist/messages/messages.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { CreateMessageDTO } from './dtos/create-message.dto';
-export declare class MessagesController {
-    getAllMessages(): {
-        id: number;
-        message: string;
-    }[];
-    createMessage(body: CreateMessageDTO): void;
-    getMessageByID(id: string): void;
-}
