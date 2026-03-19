1cec4ea0d43ebf74df33888f0caa373474e5d517
Prince Jaiswal <princejaiswal278@gmail.com>
2026-03-19 12:51:31 +0530

    changes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,23 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDTO } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    constructor(private readonly messageService: MessagesService) {}
+
     @Get()
     getAllMessages() {
-        return [
-            {id: 1, message: "Hello"},
-            {id: 2, message: "World"}
-        ]
+        return this.messageService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDTO) {
-        console.log("body", body);
+        return this.messageService.create(body.content);
     }
 
     @Get('/:id')
     getMessageByID(@Param('id') id: string) {
-        console.log("id", id)
+        return this.messageService.findOne(id);
     }
 }
